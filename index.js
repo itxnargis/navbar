18f15a091ce14b74823392487d3f51fe1e91e229
itxnargis <itxnargiskhatun@gmai.com>
2024-01-07 15:41:17 +0530

    fix navbar toggle

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
+const menuIcon = document.getElementById('menu-icon');
+const closeIcon = document.getElementById('close-icon');
 
-navToggle.addEventListener('click', function(){
+
+navToggle.addEventListener('click', function () {
     links.classList.toggle('show-links');
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-times');
+    const isMenuOpen = links.classList.contains('show-links');
+});
+
+closeIcon.addEventListener('click', function () {
+    links.classList.remove('show-links');
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
 });
